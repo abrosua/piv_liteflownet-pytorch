1b927a2817a341837ded213021f38ac0868245be
Faber Silitonga <abrosua@gmail.com>
2019-11-26 00:57:55 +0700

    fix the dependencies list

--- download.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-default.pytorch
-wget --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-kitti.pytorch
-wget --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-sintel.pytorch
