67c9c361208d355803cf7febd62348d92fb8fc3e
Faber Silitonga <abrosua@gmail.com>
2019-12-01 16:41:45 +0700

    modify the PIVData, training datasets, to use the flow transforms. And other datasets' dependencies on the transformers

--- src/datasets.py
+++ src/datasets.py
@@ -5,21 +5,25 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 
 import json
-import os, random
+import os
 from glob import glob
-from typing import Dict, Optional, List, Tuple, Union, Iterable, Any
+from typing import Optional, List, Tuple
 
-from src.utils_data import image_files_from_folder, read_gen
+from src.utils_data import image_files_from_folder, flo_files_from_folder, read_gen
+import src.flow_transforms as f_transforms
 
 
 class PIVData(Dataset):
-    def __init__(self, args, is_cropped: bool = False, root: str = '', replicates: int = 1, mode: str = 'train'
-                 ) -> None:
+    def __init__(self, args, is_cropped: bool = False, root: str = '', replicates: int = 1, mode: str = 'train',
+                 transform: Optional[object] = None) -> None:
         self.args = args
         self.is_cropped = is_cropped
         self.crop_size = args.crop_size
         self.render_size = args.inference_size
+        self.transform = transform
+
         self.replicates = replicates
+        self.set_type = mode
 
         exts = ['.jpg', '.jpeg', '.png', '.bmp', '.tif', '.ppm']
         dataset_list = sorted(glob(os.path.join(root, f'*.json')))
@@ -28,11 +32,10 @@ class PIVData(Dataset):
         self.image_list = []
 
         for dataset_file in dataset_list:
-            flonames = json_pickler(dataset_file, replicate=self.replicates, set_type=mode)
+            flonames = self._json_pickler(dataset_file)
 
             for flo in flonames:
                 if 'test' in flo:
-                    # print file
                     continue
 
                 fbase = os.path.splitext(flo)[0]
@@ -53,7 +56,7 @@ class PIVData(Dataset):
 
         self.size = len(self.image_list)
 
-        self.frame_size = read_gen(self.image_list[0][0]).shape
+        self.frame_size = read_gen(self.image_list[0][0]).size
 
         if (self.render_size[0] < 0) or (self.render_size[1] < 0) or \
                 (self.frame_size[0] % 64) or (self.frame_size[1] % 64):
@@ -68,43 +71,64 @@ class PIVData(Dataset):
         return self.size
 
     def __getitem__(self, index: int) -> Tuple[List[torch.Tensor], torch.Tensor]:
-
         index = index % self.size
 
         img1 = read_gen(self.image_list[index][0])
         img2 = read_gen(self.image_list[index][1])
         flow = read_gen(self.flow_list[index])
+        data = [[img1, img2], [flow]]
 
-        # Instantiate the image cropper
-        image_size = img1.shape[:2]
         if self.is_cropped:
-            cropper = StaticRandomCrop(image_size, self.crop_size)
+            crop_type = 'rand'
+            csize = self.crop_size
         else:
-            cropper = StaticCenterCrop(image_size, self.render_size)
+            crop_type = 'center'
+            csize = self.render_size
 
         # Instantiate the transformer
-        transformer = transforms.Compose([
-            cropper,
-            transforms.ToTensor()
-        ])
+        if self.transform is None:
+            transformer = f_transforms.Compose([
+                f_transforms.Crop(csize, crop_type=crop_type),
+                f_transforms.ModToTensor()
+            ])
+        else:
+            transformer = self.transform
 
-        img1 = transformer(img1)
-        img2 = transformer(img2)
-        flow = transformer(flow)
+        res_data = tuple(transformer(*data))
+        return res_data
 
-        return [img1, img2], flow
+    def _json_pickler(self, set_dir: str) -> List[str]:
+        modes = ['train', 'val', 'test']
+        if self.set_type not in modes:
+            raise ValueError(f'Unknown input of mode ({self.set_type})! Choose between {(" or ").join(modes)} only!')
+
+        subdir = os.path.basename(set_dir).rsplit('_', 1)[0]
+        directory = os.path.join(os.path.dirname(os.path.dirname(set_dir)), subdir)
+
+        datanames = []
+        with open(set_dir) as json_file:
+            data = json.load(json_file)[self.set_type]
+
+            for line in data:
+                bname = os.path.basename(line)
+                filename = os.path.join(directory, bname)
+
+                if os.path.isfile(filename):
+                    for i in range(self.replicates):
+                        datanames.append(filename)
+
+        return datanames
 
 
 class InferenceRun(Dataset):
-    def __init__(self, root: str = '', pair: bool = True) -> None:
-
+    def __init__(self, inference_size: Tuple = (-1, -1), root: str = '', pair: bool = True) -> None:
+        self.render_size = list(inference_size)
         file_list = image_files_from_folder(root, pair=pair)
         prev_file = None
 
         self.image_list = []
         for file in file_list:
             if 'test' in file:
-                # print file
                 continue
 
             if pair:  # Using paired images
@@ -127,6 +151,14 @@ class InferenceRun(Dataset):
 
             self.image_list += [[img1, img2]]
 
+        self.frame_size = read_gen(self.image_list[0][0]).size
+
+        if (self.render_size[0] < 0) or (self.render_size[1] < 0) or \
+                (self.frame_size[0] % 64) or (self.frame_size[1] % 64):
+            self.render_size[0] = ((self.frame_size[0]) // 64) * 64
+            self.render_size[1] = ((self.frame_size[1]) // 64) * 64
+
+        # Sanity check on the number of image pair and flow
         self.size = len(self.image_list)
 
     def __len__(self) -> int:
@@ -135,25 +167,32 @@ class InferenceRun(Dataset):
     def __getitem__(self, index: int) -> Tuple[List[torch.Tensor], str]:
         # Init.
         index = index % self.size
-
-        img1 = read_gen(self.image_list[index][0])
-        img2 = read_gen(self.image_list[index][1])
         im_name = self.image_list[index][0]
 
-        # Transform into tensor
-        img1 = transforms.ToTensor()(img1)
-        img2 = transforms.ToTensor()(img2)
+        # Cropper and totensor tranformer for the images
+        transformer = transforms.Compose([
+            transforms.CenterCrop(self.render_size),
+            transforms.ToTensor()
+        ])
 
-        return [img1, img2], im_name
+        # Read and transform file into tensor
+        imgs = []
+        for imname in self.image_list[index]:
+            img = transformer(read_gen(imname))
+            imgs.append(img)
+
+        return imgs, im_name
 
 
 class InferenceEval(Dataset):
     def __init__(self, inference_size: Tuple = (-1, -1), root: str = '') -> None:
         self.render_size = list(inference_size)
-        file_list = image_files_from_folder(root, pair=True)
 
         self.flow_list = []
         self.image_list = []
+
+
+        file_list = flo_files_from_folder(root)
         for file in file_list:
             if 'test' in file:
                 # print file
@@ -174,12 +213,12 @@ class InferenceEval(Dataset):
 
         self.size = len(self.image_list)
 
-        self.frame_size = read_gen(self.image_list[0][0]).shape
+        self.frame_size = read_gen(self.image_list[0][0]).size
 
         if (self.render_size[0] < 0) or (self.render_size[1] < 0) or \
                 (self.frame_size[0] % 64) or (self.frame_size[1] % 64):
-            self.render_size[0] = ((self.frame_size[0])//64) * 64
-            self.render_size[1] = ((self.frame_size[1])//64) * 64
+            self.render_size[0] = ((self.frame_size[0]) // 64) * 64
+            self.render_size[1] = ((self.frame_size[1]) // 64) * 64
 
         # Sanity check on the number of image pair and flow
         assert (len(self.image_list) == len(self.flow_list))
@@ -187,18 +226,17 @@ class InferenceEval(Dataset):
     def __len__(self) -> int:
         return self.size
 
-    def __getitem__(self, index: int) -> Tuple[List[torch.Tensor], torch.Tensor]:
+    def __getitem__(self, index: int) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
         # Init.
         index = index % self.size
 
         img1 = read_gen(self.image_list[index][0])
         img2 = read_gen(self.image_list[index][1])
         flow = read_gen(self.flow_list[index])
-        image_size = img1.shape[:2]
 
-        # Cropper and totensor tranformer the image and flow
+        # Cropper and totensor tranformer for the images and flow
         transformer = transforms.Compose([
-            StaticCenterCrop(image_size, self.render_size),
+            transforms.CenterCrop(self.render_size),
             transforms.ToTensor()
         ])
 
@@ -207,30 +245,49 @@ class InferenceEval(Dataset):
         img2 = transformer(img2)
         flow = transformer(flow)
 
-        return [img1, img2], flow
+        return [img1, img2], [flow]
+
+
+# -------------------- Transformer --------------------
+def get_transform(args):
+    """
+    The default transformer for the training script.
+    WARNING:
+    Args:
+        args: argument parser.
+    Returns:
+        Both training and validation data augmentation transformer.
+    """
+
+    # Training set transformer (Data Augmentation)
+    train_tranformer = f_transforms.Compose([
+        f_transforms.RandomTranslate(20),
+        f_transforms.RandomRotate(17, diff_angle=3),
+        f_transforms.RandomScale([0.9, 2.0]),
+        f_transforms.Crop(args.crop_size, crop_type='rand'),
+        f_transforms.ModToTensor(),
+        f_transforms.RandomPhotometric(
+            min_noise_stddev=0.0,
+            max_noise_stddev=0.04,
+            min_contrast=-0.8,
+            max_contrast=0.4,
+            brightness_stddev=0.2,
+            min_color=0.5,
+            max_color=2.0,
+            min_gamma=0.7,
+            max_gamma=1.5),
+    ])
+
+    # Validation set transformer (Data Augmentation)
+    val_tranformer = f_transforms.Compose([
+        f_transforms.Crop(args.crop_size, crop_type='rand'),
+        f_transforms.ModToTensor(),
+    ])
+
+    return train_tranformer, val_tranformer
 
 
 # -------------------- Helper --------------------
-class StaticRandomCrop(object):
-    def __init__(self, image_size, crop_size):
-        self.th, self.tw = crop_size
-        h, w = image_size
-        self.h1 = random.randint(0, h - self.th)
-        self.w1 = random.randint(0, w - self.tw)
-
-    def __call__(self, img):
-        return img[self.h1:(self.h1+self.th), self.w1:(self.w1+self.tw), :]
-
-
-class StaticCenterCrop(object):
-    def __init__(self, image_size, crop_size):
-        self.th, self.tw = crop_size
-        self.h, self.w = image_size
-
-    def __call__(self, img):
-        return img[(self.h - self.th)//2:(self.h + self.th)//2, (self.w - self.tw)//2:(self.w + self.tw)//2, :]
-
-
 def df_pickler(set_dir: str, replicate: int = 1, mode='json') -> List[str]:
     subdir = os.path.basename(set_dir).rsplit('_', 1)[0]
     directory = os.path.join(os.path.dirname(set_dir), subdir)
@@ -260,28 +317,6 @@ def txt_pickler(set_dir: str, replicate: int = 1) -> List[str]:
     datanames = []
     with open(set_dir) as fp:
         for line in fp:
-            filename = os.path.join(directory, line)
-
-            if os.path.isfile(filename):
-                for i in range(replicate):
-                    datanames.append(filename)
-
-    return datanames
-
-
-def json_pickler(set_dir: str, replicate: int = 1, set_type: str = 'train') -> List[str]:
-    modes = ['train', 'val', 'test']
-    if set_type not in modes:
-        raise ValueError(f'Unknown input of mode ({set_type})! Choose between {(" or ").join(modes)} only!')
-
-    subdir = os.path.basename(set_dir).rsplit('_', 1)[0]
-    directory = os.path.join(os.path.dirname(os.path.dirname(set_dir)), subdir)
-
-    datanames = []
-    with open(set_dir) as json_file:
-        data = json.load(json_file)[set_type]
-
-        for line in data:
             filename = os.path.join(directory, line)
 
             if os.path.isfile(filename):
